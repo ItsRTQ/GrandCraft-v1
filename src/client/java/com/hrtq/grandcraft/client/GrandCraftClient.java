9fd89c0b6e5920995540b2f6656337204c289fad
HCoding <titanbabrisa@gmail.com>
2026-07-29 19:19:18 -0400

    Initial try for minecraft mode

---
+++ src/client/java/com/hrtq/grandcraft/client/GrandCraftClient.java
@@ -0,0 +1,10 @@
+package com.hrtq.grandcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrandCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/hrtq/grandcraft/client/GrandCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hrtq.grandcraft.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GrandCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
